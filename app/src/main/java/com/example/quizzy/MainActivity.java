018b23ba550d44a43d78714211f536557ba2b28b
ahan-stark <ahanabubakar@gmail.com>
2022-06-23 23:09:26 +0530

    code formatted

--- app/src/main/java/com/example/quizzy/MainActivity.java
+++ app/src/main/java/com/example/quizzy/MainActivity.java
@@ -14,30 +14,31 @@ import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MainActivity extends AppCompatActivity {
-    TextView firstNum, secondNum, disResult, userRank,lastScore;
+    TextView firstNum, secondNum, disResult, userRank, lastScore;
     EditText enteredAnswer;
     Button buttonClick;
     int userFinalScore;
     AtomicInteger attemptTime = new AtomicInteger();
     Random random = new Random();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sharedPreferences = getSharedPreferences("MySharePref",Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharePref", Context.MODE_PRIVATE);
         lastScore = findViewById(R.id.prevScore);
-        firstNum =  findViewById(R.id.firstNumber);
+        firstNum = findViewById(R.id.firstNumber);
         secondNum = findViewById(R.id.secondNumber);
         generateRandomNumbers();
         disResult = findViewById(R.id.finalResult);
         userRank = findViewById(R.id.userScore);
         buttonClick = findViewById(R.id.checkButton);
-        firstNum =  findViewById(R.id.firstNumber);
+        firstNum = findViewById(R.id.firstNumber);
         secondNum = findViewById(R.id.secondNumber);
-        enteredAnswer =(EditText)findViewById(R.id.enterAnswer);
+        enteredAnswer = (EditText) findViewById(R.id.enterAnswer);
         //fetching previous score from sharedPreference file
-        int sc = sharedPreferences.getInt("prevScore",0);
-        lastScore.setText(""+sc);
+        int sc = sharedPreferences.getInt("prevScore", 0);
+        lastScore.setText("" + sc);
         buttonClick.setOnClickListener(view -> {
             String userAnswer = enteredAnswer.getText().toString();
             attemptTime.set(attemptTime.get() + 1);
@@ -45,49 +46,46 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
+
     private void checkResults(String userAnswer) {
-       String stringRandomNumber1 = firstNum.getText().toString();
-       String stringRandomNumber2 = secondNum.getText().toString();
-        int actualValue = Integer.parseInt(stringRandomNumber1) +  Integer.parseInt(stringRandomNumber2);
+        String stringRandomNumber1 = firstNum.getText().toString();
+        String stringRandomNumber2 = secondNum.getText().toString();
+        int actualValue = Integer.parseInt(stringRandomNumber1) + Integer.parseInt(stringRandomNumber2);
         int userValueInt = Integer.parseInt(userAnswer);
         try {
-            if(actualValue == userValueInt)
-            {
+            if (actualValue == userValueInt) {
                 userFinalScore = userFinalScore + 1;
                 disResult.setText("you are right!");
-                userRank.setText(""+ userFinalScore);
-                if(Integer.parseInt(userRank.getText().toString()) == 3){
-                    SharedPreferences sharedPreferences = getSharedPreferences("MySharePref",Context.MODE_PRIVATE);
+                userRank.setText("" + userFinalScore);
+                if (Integer.parseInt(userRank.getText().toString()) == 3) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("MySharePref", Context.MODE_PRIVATE);
                     int finalPreviousAttempt = attemptTime.get();
-                    Toast.makeText(this, "You have scored 10 points out of "+attemptTime.get()+"attempts", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "You have scored 10 points out of " + attemptTime.get() + "attempts", Toast.LENGTH_SHORT).show();
                     userFinalScore = 0;
                     //inserting previous score to sharedPreferences
-                    sharedPreferences.edit().putInt("prevScore",finalPreviousAttempt).commit();
-                    int sc = sharedPreferences.getInt("prevScore",0);
-                    lastScore.setText(""+sc);
-                    userRank.setText(""+userFinalScore);
+                    sharedPreferences.edit().putInt("prevScore", finalPreviousAttempt).commit();
+                    int sc = sharedPreferences.getInt("prevScore", 0);
+                    lastScore.setText("" + sc);
+                    userRank.setText("" + userFinalScore);
 
                     attemptTime.set(0);
                     generateRandomNumbers();
-                }
-                else {
+                } else {
                     generateRandomNumbers();
                     enteredAnswer.getText().clear();
                 }
-            }
-            else {
+            } else {
                 disResult.setText("oops! try again");
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-    private void generateRandomNumbers(){
-        int randomFirstNumber =  random.nextInt(10);
-        int randomSecondNumber =  random.nextInt(10);
+    private void generateRandomNumbers() {
+        int randomFirstNumber = random.nextInt(10);
+        int randomSecondNumber = random.nextInt(10);
         String stringRandomNumber1 = String.valueOf(randomFirstNumber);
         String stringRandomNumber2 = String.valueOf(randomSecondNumber);
         firstNum.setText(stringRandomNumber1);
--- app/src/main/java/com/example/quizzy/Splash.java
+++ app/src/main/java/com/example/quizzy/Splash.java
@@ -12,16 +12,16 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         int SPLASH_SCREEN_TIME_OUT = 2000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this,MainActivity.class);
+                Intent i = new Intent(Splash.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
-        },SPLASH_SCREEN_TIME_OUT);
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
