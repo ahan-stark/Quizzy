552bb9a9380693f4592eaeacc3c35b1aff852e1e
ahan-stark <ahanabubakar@gmail.com>
2022-06-23 23:08:17 +0530

    added sharedPreference

---
+++ app/src/main/java/com/example/quizzy/Splash.java
@@ -0,0 +1,27 @@
+package com.example.quizzy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        int SPLASH_SCREEN_TIME_OUT = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
